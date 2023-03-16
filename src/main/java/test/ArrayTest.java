7c54bb26f5b303fd8d18486632e030cc5e744bb1
xiaozhao <782717466@qq.com>
2023-03-16 10:09:53 +0800

    2023.3.15 数组测试

---
+++ src/main/java/test/ArrayTest.java
@@ -0,0 +1,9 @@
+package test;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[2];
+        int i = 0;
+        System.out.println(arr[i]);
+    }
+}
